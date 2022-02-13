e051a325b10cdabf0fdb244629fb103edcada0d8
Steffen Grahl <steffen@sgwebservice.de>
2022-02-13 12:15:37 +0100

    Introduce Ranking entity

--- src/src/Repository/RankingRepository.php
+++ src/src/Repository/RankingRepository.php
@@ -2,7 +2,21 @@
 
 namespace App\Repository;
 
-class RankingRepository
+use App\Entity\Ranking;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Ranking|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Ranking|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Ranking[]    findAll()
+ * @method Ranking[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RankingRepository extends ServiceEntityRepository
 {
 
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ranking::class);
+    }
 }
