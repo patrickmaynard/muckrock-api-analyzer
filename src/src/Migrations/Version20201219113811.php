463dd896909f85605943ab8826e15358d595a666
patrickmaynard <patrickmaynard@users.noreply.github.com>
2022-06-09 21:56:51 +0200

    Make migrations more flexible

--- src/src/Migrations/Version20201219113811.php
+++ src/src/Migrations/Version20201219113811.php
@@ -22,7 +22,7 @@ final class Version20201219113811 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE major_city (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, absolute_url VARCHAR(1000) NOT NULL, average_response_time INT DEFAULT NULL, success_rate DOUBLE PRECISION DEFAULT NULL, last_update DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE IF NOT EXISTS major_city (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, absolute_url VARCHAR(1000) NOT NULL, average_response_time INT DEFAULT NULL, success_rate DOUBLE PRECISION DEFAULT NULL, last_update DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
